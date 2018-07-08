faecdbe02f8bec240fc8a873be72e8a2784b517f
0x3A2B <waq5858@hotmail.com>
2018-07-08 16:02:47 +0800

    完成ID读取

--- ESP8266/door.lua
+++ ESP8266/door.lua
@@ -1,15 +1,15 @@
 srv = net.createConnection(net.TCP, 0)
-srv:on("receive", function(sck, c) print(c) end)
+srv:on("receive", function(sck, c) sck:send("OK,GOTIT") print(c)end)
 -- Wait for connection before sending.
 srv:on("connection", function(sck, c)
   -- 'Connection: close' rather than 'Connection: keep-alive' to have server 
   -- initiate a close of the connection after final response (frees memory 
   -- earlier here), https://tools.ietf.org/html/rfc7230#section-6.6 
-  sck:send("GET /get HTTP/1.1\r\nHost: httpbin.org\r\nConnection: close\r\nAccept: */*\r\n\r\n")
+  sck:send("smartdoocconnect")
 end)
 srv:connect(9999,"172.16.2.223")
 -- user defined function: read from reg_addr content of dev_addr
-function pn532_send(dev_addr,data)
+function pn532_send(dev_addr,data, len)
     i2c.start(id)
     i2c.address(id, dev_addr, i2c.TRANSMITTER)
     i2c.write(id, data)
@@ -18,18 +18,44 @@ function pn532_send(dev_addr,data)
     --i2c.write(id, encoder.fromHex("0000FF03FDD414011700")) 
     --i2c.write(id, encoder.fromHex("0000FF04FCD44A0200E000")) 
     i2c.stop(id)
+    --get ack
     i2c.start(id)
     i2c.address(id, dev_addr, i2c.RECEIVER)
-    c = i2c.read(id, 25)
+    f = i2c.read(id, 8)
     i2c.stop(id)
+    --print(encoder.toHex(f))
 
+   tmr.create():alarm(100, tmr.ALARM_SINGLE, function()
+      i2c.start(id)
+      i2c.address(id, dev_addr, i2c.RECEIVER)
+      c = i2c.read(id, len+2)
+      i2c.stop(id)
+   end)
+   
     return encoder.toHex(c)
 end
-reg = pn532_send(0x24,encoder.fromHex("0000FF03FDD414011700"))
 function get_id()
-   c = pn532_send(0x24, encoder.fromHex("0000FF04FCD44A0200E000"))
+   c = pn532_send(0x24, encoder.fromHex("0000FF04FCD44A0200E000"), 20)
    return string.sub(c, 29 , 36)
 end
 
-reg = get_id()
-print(reg)
+sendid = function(T)
+   last_id = c
+   c = get_id()
+   if(c ~= "80808080") then
+      print(c)
+      --srv:send("ID--" .. c)
+   end
+
+   --- open the door
+
+
+   
+   scancard:start()
+end
+
+pn532_send(0x24,encoder.fromHex("0000FF05FBD4140114000300"), 8)
+
+scancard = tmr.create()
+scancard:register(200, tmr.ALARM_SEMI, sendid)
+scancard:start()
